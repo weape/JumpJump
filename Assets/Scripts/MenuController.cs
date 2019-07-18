afafa06d7c0456d173005d34157f82f8fad1a602
nickyc975 <chenjinlong2016@gmail.com>
2019-07-18 17:21:02 +0800

    Added a scene.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void OnBackCLicked()
+    {
+        Application.LoadScene("Menu");
+    }
+
+    public void OnPlayClicked()
+    {
+        Application.LoadScene("Game");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
