f80b224a2abbbae3425c5bb97c7af56fbec1d643
Mark <f18846188605@gmail.com>
2019-07-25 17:19:13 +0800

    Improved experience

--- Assets/Scripts/GroundController.cs
+++ Assets/Scripts/GroundController.cs
@@ -18,13 +18,14 @@ public class GroundController : MonoBehaviour
 
     private int groundLength = 75;
 
-    private int templateCount = 5;
+    private int templateCount = 20;
 
     private Vector3 posDelta = new Vector3(5.0F, 0.0F, 0.0F);
 
     // Start is called before the first frame update
     void Start()
     {
+        float totalTemplateLength = 15;
         System.Random rand = new System.Random();
         // load all game templates
         Object[] Templates = Resources.LoadAll("template");
@@ -38,12 +39,17 @@ public class GroundController : MonoBehaviour
         chest = Instantiate(chest, grass.transform.position + new Vector3(0.0F, 2.0F, 0.0F), grass.transform.rotation, transform);
 
         // set game template into scene
-        GameObject template;
-        template = (GameObject)Instantiate(Templates[rand.Next(0, Templates.Length)], new Vector3(rand.Next(10, 30), -3.0F, 0.0F), transform.rotation, transform);
-        for (int i = 0; i < templateCount; i++)
+        GameObject preTemplate, template;
+        preTemplate = (GameObject)Instantiate(Templates[rand.Next(0, Templates.Length)], new Vector3(rand.Next(10, 30), -3.0F, 0.0F), transform.rotation, transform);
+        for (int i = 0; i < templateCount - 1; i++)
         {
-            Vector3 pos = new Vector3(rand.Next(30, 50), 0.0F, 0.0F);
-            template = Instantiate((GameObject)Templates[rand.Next(0, Templates.Length)], template.transform.position + pos, transform.rotation, transform);
+            Vector3 pos = new Vector3(preTemplate.GetComponent<BoxCollider2D>().size.x, 0.0F, 0.0F);
+            totalTemplateLength += preTemplate.GetComponent<BoxCollider2D>().size.x;
+            template = (GameObject)Templates[rand.Next(0, Templates.Length)]; // choose one template by random
+            if (totalTemplateLength + template.GetComponent<BoxCollider2D>().size.x < grass.transform.position.x)
+                preTemplate = Instantiate(template, preTemplate.transform.position + pos, transform.rotation, transform);
+            else
+                break;
         }
 
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
